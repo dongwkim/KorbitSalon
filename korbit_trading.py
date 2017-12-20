94c0f50d0e54a2413123db33f588e0c415582e54
dongwkim <dongwkim@DONGWKIM-KR.kr.oracle.com>
2017-12-20 15:27:21 +0900

    basic test

--- korbit_trading.py
+++ korbit_trading.py
@@ -1,23 +1,29 @@
 from  korbit_api import *
 import time
+#import logging
 
 
 if __name__ == "__main__":
 
-    pooling()
-    token = getAccessToken()
-    header = {"Authorization": "Bearer " + token['access_token']}
+    #logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s',filename='trading.trc',level=logging.DEBUG)
+    #logger = logging.getLogger('korbit_trading')
 
-    # Ticker
-    stime = time.time()
-    ticker = get('ticker/detailed', currency_pair='xrp_krw')
-    elapsed = int((time.time() - stime) * 1000)
-    print "Ticker: {}ms".format(elapsed)
+    pooling()
+    token = getAccessToken('C:\Users\dongwkim\Keys\korbit_key.csv')
+    header = {"Authorization": "Bearer " + token['access_token']}
 
     balance = chkUserBalance('krw',header)
 
 
     for i in range(10):
+
+        #ticker
+        stime = time.time()
+        ticker = get('ticker/detailed', currency_pair='xrp_krw')
+        elapsed = int((time.time() - stime) * 1000)
+        print "Ticker: {}ms".format(elapsed)
+
+        #buy order
         mybid = {"currency_pair" : "xrp_krw", "type":"limit", "price": "700", "coin_amount":"10", "nonce": getNonce()}
         stime = time.time()
         bidorder = bidOrder(mybid, header)
@@ -25,12 +31,14 @@ if __name__ == "__main__":
         #print order
         print "{} | {} {:7s}: id# {:10s} is {:15s} {:3d}ms".format(getStrTime(stime),bidorder['currencyPair'],'Buy',str(bidorder['orderId']) ,bidorder['status'], elapsed)
 
+        #sell order
         stime = time.time()
         myask = {"currency_pair" : "xrp_krw", "type":"limit", "price": "710", "coin_amount":"10", "nonce": getNonce()}
         askorder = askOrder(myask,header)
         elapsed = int((time.time() - stime) * 1000)
         print "{} | {} {:7s}: id# {:10s} is {:15s} {:3d}ms".format(getStrTime(stime),askorder['currencyPair'],'Sell',str(askorder['orderId']) ,askorder['status'], elapsed)
 
+        #cancel order
         stime = time.time()
         mycancel = {"currency_pair": bidorder['currencyPair'], "id": bidorder['orderId'],"nonce":getNonce()}
         cancel = cancelOrder(mycancel,header)
