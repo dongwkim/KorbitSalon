972540fc8fdf2b79af36bedcf4491cdce25ec541
dongwkim <dongwkim@DONGWKIM-KR.kr.oracle.com>
2018-01-03 15:37:25 +0900

    merge to master

--- TokenManager.py
+++ TokenManager.py
@@ -6,26 +6,27 @@ import redis
 import time
 import logging
 from platform import system
+import os
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s %(message)s',filename='tracelog/tokenmanager.trc',level=logging.INFO)
 logger = logging.getLogger('TokenManager')
 
-class UserSessionInfo:
-    def __init__(self, pSecFilePath, myid, host = 'localhost', port = '6379' ):
+class TokenManager:
+    def __init__(self, pSecFilePath, pRedisHost, pRedisPort, pRedisUser, pRedisPassword ):
         ''' redis connection
             pSecFilePath : Cryptocurrency API key/secret filename
             host         : redis host(default=localhost)
             port         : redis port(default=6379)
         '''
         self.secFilePath=pSecFilePath
-        self.myid = myid
+        self.myid = pRedisUser
         self.accessInfo = {}
         self.authAtt = ['key','secret','email','password']
         self.urlPrefix = 'https://api.korbit.co.kr/v1'
         self.mySession= requests.Session()
         #self.myToken = 4*[None]
         self.myToken = {}
-        self.redisCon = redis.StrictRedis(host=host, port=port, db=0,charset="utf-8", decode_responses=True)
+        self.redisCon = redis.StrictRedis(host=pRedisHost, port=pRedisPort, db=0, password=pRedisPassword,charset="utf-8", decode_responses=True)
         #self.redisCon = redis.StrictRedis(host='localhost', port=6379, db=0,charset="utf-8", decode_responses=True)
 
     def readSecFile(self):
@@ -55,6 +56,7 @@ class UserSessionInfo:
         self.redisCon.hmset(self.myid, self.accessInfo)
         self.redisCon.hmset(self.myid, self.myToken)
         print("{} | Insert Token to Redis".format(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())))
+        time.sleep(3000)
 
     def updateTokenOnRedis(self):
         ''' Update token on redis
@@ -82,7 +84,7 @@ class UserSessionInfo:
         return str(self.redisCon.hmget(self.myid,'refresh_token')[0])
 
     def getAccessToken(self):
-        return str(self.redisCon.hmget(self.myid,'access_token')[0])
+        return str(self.redisCon.hmget(self.redisUser,'access_token')[0])
 
     def getExpiredTime(self):
         return str(self.redisCon.hmget(self.myid,'expires_in')[0])
@@ -93,18 +95,29 @@ class UserSessionInfo:
         print(self.redisCon.hmget(self.myid, 'refresh_token')[0])
 
 if __name__ == "__main__":
+    redisUser = os.environ['redisUser']
+    redisPassword = 'RlawjddmsrotoRl#12'
+
     if system() is 'Windows':
         secFilePath='c:/Users/dongwkim/Keys/korbit_key.csv'
+        print("windows")
     ## Linux
     else:
-        secFilePath='/usb/s1/key/korbit_key.csv'
-    redisUser = 'dongwkim'
-    #secFilePath="c:/Users/dongwkim/keys/korbit_key.csv"
-    mySession=UserSessionInfo(secFilePath, 'dongwkim','cryptosalon.iptime.org', '6379')
+        if (redisUser == "kiwonyoon"):
+            secFilePath='/seckeys/kiwonyoon.csv'
+            redisHost = 'localhost'
+            redisPort = 16379
+        elif (redisUser == "dongwkim"):
+            secFilePath='/usb/s1/key/korbit_key.csv'
+            redisHost = 'localhost'
+            redisPort = 16379
+        else:
+            print("Critical Error")
+
+    mySession=TokenManager(secFilePath, redisHost, redisPort, redisUser, redisPassword)
     mySession.readSecFile()
-    # #print  mySession.accessInfo
-    # #
+
     mySession.myToken = mySession.doPost('oauth2/access_token', client_id=mySession.accessInfo['key'], client_secret=mySession.accessInfo['secret'], username=mySession.accessInfo['email'], password=mySession.accessInfo['password'], grant_type='password')
+    print(mySession.myToken)
     mySession.insertTokenIntoRedis()
     mySession.updateToken()
-    #print type(str(mySession.getAccessToken()))
