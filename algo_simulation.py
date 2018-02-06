b1bbb1586aa27b07a2b11595b78f2610cb47021b
dongwkim <dotrry@gmail.com>
2018-02-06 15:39:47 +0900

    multi orderable simulation

--- algo_simulation.py
+++ algo_simulation.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     coin = 'xrp'
     limit = 0.95
     currency = 'xrp_krw'
-    debug = True
+    debug = False
     debug_data = False
     debug_time = False
     total_bidding = 0
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     algo_priority = {"Big Slump":10, "Midium Slump":9, "Little Slump": 8, "Baby Slump":7}
     ## multi trader for water ride
     #traders = {'dongwkim-trader1':False, 'dongwkim-trader2':False,'dongwkim-trader3': False}
-    num_traders = 3
+    num_traders = 5
     traders = OrderedDict()
     for i in range(num_traders):
         traders[redisUser+'-trader'+str(i+1)] = False
@@ -93,8 +93,8 @@ if __name__ == "__main__":
         iTimestamp = sTimestamp[5]
         timestampBucker.append(iTimestamp)
 
-    start_time = xrpm.getEpochTime('2018-01-16 08:20:00')
-    end_time = xrpm.getEpochTime('2018-01-16 20:00:00')
+    start_time = xrpm.getEpochTime('2018-02-04 00:00:00')
+    end_time = xrpm.getEpochTime('2018-02-05 23:00:00')
     myTimestamp.sort()
     if debug_data:
         pass
@@ -107,15 +107,14 @@ if __name__ == "__main__":
         if debug_data:
             print("end pos:{}".format(end_pos))
     except StopIteration:
-        print("ERROR | Start / End Time is not in redis data")
+        print("\033[93mERROR | Start / End Time is not in redis data\033[0m")
         raise
     print("{:20s} | Total {} tickers will be simulated.".format(xrpm.getStrTime(time.time() * 1000), end_pos - start_pos))
 
     loop_outsider_timer = time.time()
 
-    for ptime in timestampBucker[start_pos:end_pos]:
+    for ptime in timestampBucker[start_pos:end_pos:3]:
 
-        print("{:20s} | Processing.. ".format(xrpm.getStrTime(int(ptime))), end="\r")
     #while start_time <= end_time:
 
         loop_insider_timer = time.time()
@@ -179,8 +178,9 @@ if __name__ == "__main__":
             ######################################
             ##  Switch Traders
             ######################################
-            if water_ride_enable and xrpm.trading and buy_price * water_ride_ratio > int(ticker['last']):
+            if water_ride_enable and xrpm.trading and xrpm.buy_price * water_ride_ratio > int(ticker['last']):
                 try:
+                    pre_sell_price = xrpm.sell_price
                     # find available trader from dictionary, search values is False
                     c_trader = next(i for i in range(len(traders)) if list(traders.values())[i] is False)
                     print("{:20s} | Spawn Water Rider trader. Current Trader is {}".format(xrpm.getStrTime(int(ptime)), list(traders)[c_trader]))
@@ -192,76 +192,77 @@ if __name__ == "__main__":
                     if debug:
                         print("{:20s} | DEBUG | No more avalilable traders".format(xrpm.getStrTime(time.time()*1000)))
                     pass
+            #release spawned trader if last is greater than prior pre_sell_price * ratio
+            elif water_ride_enable and c_trader != 0 and not xrpm.trading and pre_sell_price * 0.97 <= int(ticker['bid']) :
+                    traders[list(traders)[c_trader]] = False
+                    print("\033[90m{:20s} | Turn off Spawned Water Rider trader\033[0m".format(xrpm.getStrTime(time.time()*1000)))
+                    ## Set Trader
+                    c_trader = xrpm.setSellTrader(traders,myorderlist)
+
+
 
 
             # Create New Algorithm Instance
             myalgo = algo.algo(tx_1min_stat, tx_10min_stat, tx_hr_stat, ticker)
 
             # Put your Algorithm here
-            if not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and  myalgo.slump(15, 0.5, 5, 2.0, -9999):
-                print("{:20s} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
+            if not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and  myalgo.slump(15, 0.9, 5, 2.0, -9999):
+                print("{:20s} | \033[95mHit {} Algorithm\033[0m | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
                 .format(xrpm.getStrTime(int(ptime)), "Big Slump", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
                 xrpm.bidding = True
-                benefit = 0.07
+                benefit = 0.45
                 algorithm = 'Big Slump'
             ## Midium Slump Algorithm
-            elif not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and  myalgo.slump(10, 0.4, 4, 1.5 , -9999 ):
+            elif not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and  myalgo.slump(10, 0.9, 4, 1.3 , -9999 ):
                 print("{:20s} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
                 .format(xrpm.getStrTime(int(ptime)), "Midium Slump", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
                 xrpm.bidding = True
-                benefit = 0.04
+                benefit = 0.032
                 algorithm = 'Midium Slump'
             ## Little Slump Algorithm
-            elif not xrpm.bidding and myalgo.basic(95) and myalgo.slump(10, 0.3, 3, 1.3 , -9999 ):
-                print("{:20s} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
+            elif not xrpm.bidding and myalgo.basic(95) and myalgo.slump(10, 0.9, 3, 1.2 , -9999 ):
+                print("{:20s} | \033[95mHit {} Algorithm\033[0m | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
                 .format(xrpm.getStrTime(int(ptime)), "Little Slump", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
                 xrpm.bidding = True
-                benefit = 0.025
+                benefit = 0.022
                 algorithm = 'Little Slump'
+                if c_trader != 0:
+                    benefit = 0.032
             ## Baby Slump Algorithm
             elif not xrpm.bidding and not xrpm.trading \
-              and myalgo.slump(7, 0.15, 2.5, 4.0 , -9999 ) and myalgo.basic(97) :
-                print("{:20s} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
+              and myalgo.slump(7, 0.9, 2.0, 4.0 , -9999 ) and myalgo.basic(97) :
+                print("{:20s} | \033[95mHit {} Algorithm\033[0m | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
                 .format(xrpm.getStrTime(int(ptime)), "Baby Slump", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
                 xrpm.bidding = True
-                benefit = 0.015
+                benefit = 0.032
                 algorithm = 'Baby Slump'
-            ## UpDown Slump Algorithm
-            elif not xrpm.bidding and not xrpm.trading and myalgo.basic(98) and myalgo.slump(7, 0.2, 2, -2.0 , 0 ):
-                #print("{} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
-                #.format(xrpm.getStrTime(int(ptime)), "UpDown Slump", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
-                xrpm.bidding = False
-                benefit = 0.012
-            elif not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and myalgo.rise(0.2, 1, 1, 1, 3 ):
-                #print("{} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
-                #.format(xrpm.getStrTime(int(ptime)), "Rise", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
-                #elif not testing and not xrpm.trading and last < high * limit and   myalgo.rise(0.2, 1, 1, 1.0, 3 ):
-                xrpm.bidding = False
-                benefit = 0.01
-            elif not xrpm.bidding and not xrpm.trading and myalgo.basic(95) and myalgo.zigzag( -0.07, 0.2, 0.5, 0.5 ):
-                print("{} | Hit {} Algorithm | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
-                .format(xrpm.getStrTime(int(ptime)), "Rise", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
-                #elif not testing and not xrpm.trading and last < high * limit and   myalgo.rise(0.2, 1, 1, 1.0, 3 ):
-                xrpm.bidding = False
-                benefit = 0.01
+            ## Regresssion
+            elif not xrpm.bidding and not xrpm.trading \
+              and myalgo.reg(3,-2) and myalgo.basic(97) :
+                print("{:20s} | \033[95mHit {} Algorithm\033[0m | price:{} delta:{}/{}/{} avg:{:4.0f}/{:4.0f}"\
+                .format(xrpm.getStrTime(int(ptime)), "Avg Regression", ticker['last'], tx_hr_stat['tx_hr_price_delta'], tx_10min_stat['tx_10min_price_delta'], tx_1min_stat['tx_1min_price_delta'], tx_hr_stat['tx_hr_price_avg'], tx_10min_stat['tx_10min_price_avg']))
+                xrpm.bidding = True
+                benefit = 0.013
+                algorithm = 'Avg Regression'
 
 
             #Buy Position
             if not xrpm.trading and xrpm.bidding:
-                buy_price = int(ticker['ask'])
-                sell_price = buy_price + int(buy_price * benefit)
+                xrpm.buy_price = int(ticker['ask'])
+                xrpm.sell_price = xrpm.buy_price + int(xrpm.buy_price * benefit)
                 # if ask volume is less than my volume what we do next?
-                buy_volume = int(money) // buy_price
-                sell_volume = buy_volume * 0.998
+                buy_volume = int(money) // xrpm.buy_price
+                xrpm.sell_volume = buy_volume * 0.998
                 order_id = '12345'
                 xrpm.trading = True
                 xrpm.bidding = True
-                order_savepoint = {"type": "bid", "orderid" : order_id, "sell_volume" : sell_volume, "sell_price": sell_price, "currency_pair": currency, "algorithm" : algorithm, "trading": xrpm.trading, "bidding": xrpm.bidding, "money": money, "buy_price": buy_price }
+                order_savepoint = {"type": "bid", "orderid" : order_id, "sell_volume" : xrpm.sell_volume, "sell_price": xrpm.sell_price, "currency_pair": currency, "algorithm" : algorithm, "trading": xrpm.trading, "bidding": xrpm.bidding, "money": money, "buy_price": xrpm.buy_price }
                 xrpm.saveTradingtoRedis(list(traders)[c_trader],order_savepoint)
 
-                print("{:20s} * {} Buy {} coins at price {}, will sell {} won ".format(xrpm.getStrTime(int(ptime)),list(traders)[c_trader], buy_volume, ticker['ask'], sell_price))
+                print("\033[93m{:20s} * {} Buy {} coins at price {}, will sell {} won \033[0m".format(xrpm.getStrTime(int(ptime)),list(traders)[c_trader], buy_volume, ticker['ask'], xrpm.sell_price))
                 ## Multi Trader
-                myorderlist.append({"trader": list(traders)[c_trader], "sell_price": sell_price, "sell_volume": sell_volume})
+                myorderlist.append({"trader": list(traders)[c_trader], "sell_price": xrpm.sell_price, "sell_volume": xrpm.sell_volume})
+                #print("\033[92m{:20s} | {}\033[0m ".format(xrpm.getStrTime(int(ptime)),myorderlist))
                 c_trader = xrpm.setSellTrader(traders,myorderlist)
 
                 #print("{:20s} | {} : Bid Order# {} is completed.".format(xrpm.getStrTime(time.time()*1000),list(traders)[c_trader], order_id))
@@ -269,8 +270,8 @@ if __name__ == "__main__":
                 buy_time = int(ptime)
 
             #Sell Position
-            if xrpm.trading and int(ticker['last']) >= sell_price and int(ticker['bid']) >= sell_price:
-                earn_money = int(((int(ticker['bid']) - buy_price) * sell_volume) * (1 - marker_fee))
+            if xrpm.trading and int(ticker['last']) >= xrpm.sell_price and int(ticker['bid']) >= xrpm.sell_price:
+                earn_money = int(((int(ticker['bid']) - xrpm.buy_price) * xrpm.sell_volume) * (1 - marker_fee))
                 cummulative_earn_money += earn_money
                 order_id = '54321'
                 bidding_count += 1
@@ -279,22 +280,25 @@ if __name__ == "__main__":
                 sell_time = int(ptime)
                 elapsed = sell_time - buy_time
                 tx_time_list.append((xrpm.getStrTime(buy_time) , xrpm.getStrTime(sell_time), elapsed))
-                order_savepoint = {"type": "ask", "orderid" : order_id, "hell_volume" : sell_volume, "sell_price": sell_price, "currency_pair": currency, "algorithm" : algorithm, "trading": xrpm.trading, "bidding": xrpm.bidding, "money": money, "buy_price":buy_price }
+                order_savepoint = {"type": "ask", "orderid" : order_id, "hell_volume" : xrpm.sell_volume, "sell_price": xrpm.sell_price, "currency_pair": currency, "algorithm" : algorithm, "trading": xrpm.trading, "bidding": xrpm.bidding, "money": money, "buy_price":xrpm.buy_price }
                 xrpm.saveTradingtoRedis(list(traders)[c_trader],order_savepoint)
 
                 ## Remove element from myorderlist
                 del myorderlist[next(index for index,d in enumerate(myorderlist) if d['trader'] == list(traders)[c_trader])]
                 ## Set trader free
                 traders[list(traders)[c_trader]] = False
+                #print("\033[92m{:20s} | {}\033[0m ".format(xrpm.getStrTime(int(ptime)),myorderlist))
                 #print("{:20s} | {} : ask Order is completed".format(xrpm.getStrTime(int(ptime)),list(traders)[c_trader]))
-                print("{:20s} * {} Sell {} coins at price {}. earn {} won | Elapsed :{} ".format(xrpm.getStrTime(int(ptime)),list(traders)[c_trader], sell_volume, ticker['bid'], earn_money, elapsed))
+                print("\033[91m{:20s} * {} Sell {} coins at price {}. earn {} won | Elapsed :{}\033[0m ".format(xrpm.getStrTime(int(ptime)),list(traders)[c_trader], xrpm.sell_volume, ticker['bid'], earn_money, elapsed))
                 c_trader = xrpm.setSellTrader(traders,myorderlist)
+
+        print("{:20s} | Processing.. price: {} ".format(xrpm.getStrTime(int(ptime)),mystat['last']), end="\r")
 
 
 
     ## Simulation Report
     if xrpm.trading:
-        pending_tx_price = int((int(ticker['last']) - buy_price ) * buy_volume)
+        pending_tx_price = int((int(ticker['last']) - xrpm.buy_price ) * xrpm.sell_volume)
         print("Simulation Finished! You were bidding {} times and hold unselled order. Earn {} won, pending {} won".format(bidding_count, cummulative_earn_money, pending_tx_price))
         getTxList(tx_time_list)
     elif not xrpm.trading:
